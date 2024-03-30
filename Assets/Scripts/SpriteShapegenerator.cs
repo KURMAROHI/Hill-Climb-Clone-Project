af347ee2546997af8cf1dc9d2d444992e5f3c5bd
KURMA RAO KOMARA <komarakurmarao@gmail.com>
2024-03-30 11:20:05 +0530

    Generated Background using Sprite Sheet Controller And making Coin prefabs
    1)Using Scripting And Sprite Sheet Controller created Background.
    2)Created Coins prefabs And Attached required Components.

--- Assets/Scripts/SpriteShapegenerator.cs
+++ Assets/Scripts/SpriteShapegenerator.cs
@@ -6,13 +6,16 @@ using UnityEngine.U2D;
 public class SpriteShapegenerator : MonoBehaviour
 {
     [SerializeField] SpriteShapeController _spriteShapeController;
-    [SerializeField][Range(1f, 1000f)] int levellenegth = 500;
-    [SerializeField][Range(1f, 100f)] float _Xmultiplier = 4;
+    [SerializeField][Range(1f, 1000f)] int levellenegth = 250;
+    [SerializeField][Range(1f, 100f)] float _Xmultiplier = 2;
+    [SerializeField][Range(1f, 100f)] float DistanceBetweenPoints = 2;
     [SerializeField][Range(1f, 100f)] float _ymultiplier = 2;
     [SerializeField][Range(0f, 1f)] float CurveSmoothNess = 0;
 
     [SerializeField] float NoiseStep = 0.5f;
-    [SerializeField] float Bottom = 10f;
+
+    [Range(1f, 4f)]
+    [SerializeField] float _lefttangetvalue = 1f, _RightTangetValue = 1f;
 
     private Vector3 Lastpos;
 
@@ -23,20 +26,21 @@ public class SpriteShapegenerator : MonoBehaviour
         //_spriteShapeController.spline.Clear();
 
         //Debug.Log("point At 2|" + _spriteShapeController.spline.GetPosition(2) + "|::|" + _spriteShapeController.spline.GetPosition(3));
-        _spriteShapeController.spline.SetPosition(2, _spriteShapeController.spline.GetPosition(2) + Vector3.right * 100);
-        _spriteShapeController.spline.SetPosition(3, _spriteShapeController.spline.GetPosition(3) + Vector3.right * 100);
+        _spriteShapeController.spline.SetPosition(2, _spriteShapeController.spline.GetPosition(2) + Vector3.right * levellenegth * DistanceBetweenPoints);
+        _spriteShapeController.spline.SetPosition(3, _spriteShapeController.spline.GetPosition(3) + Vector3.right * levellenegth * DistanceBetweenPoints);
+        //  _spriteShapeController.spline.SetPosition(0, _spriteShapeController.spline.GetPosition(0) + new Vector3(-1,10f,0));
 
         for (int i = 0; i < levellenegth; i++)
         {
-            float _Xpos = _spriteShapeController.spline.GetPosition(i + 1).x + _Xmultiplier;
+            float _Xpos = _spriteShapeController.spline.GetPosition(i + 1).x + DistanceBetweenPoints;
             Lastpos = new Vector3(_Xpos, Mathf.PerlinNoise(0, UnityEngine.Random.Range(1f, 10f) * i) * _ymultiplier);
             _spriteShapeController.spline.InsertPointAt(i + 2, Lastpos);
 
-            if (i != 0 && i != levellenegth - 1)
+            if (i != 0 && i != levellenegth - 1 && i != 1 && i != levellenegth - 2)
             {
                 _spriteShapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-                _spriteShapeController.spline.SetLeftTangent(i, Vector3.left);
-                _spriteShapeController.spline.SetRightTangent(i, Vector3.right);
+                _spriteShapeController.spline.SetLeftTangent(i, new Vector3(-_lefttangetvalue, 0f, 0f));
+                _spriteShapeController.spline.SetRightTangent(i, new Vector3(_RightTangetValue, 0f, 0f));
             }
 
         }
