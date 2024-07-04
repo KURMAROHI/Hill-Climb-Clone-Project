af7ff2ec2e33c46e662ff3f0c46baa5ee22caed3
KURMA RAO KOMARA <komarakurmarao@gmail.com>
2024-07-04 21:39:57 +0530

    Working on main menu panel Scroll

---
+++ Assets/Scripts/MenuPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanel : MonoBehaviour
+{
+   public void OnValueChanged(Vector2 a)
+   {
+        Debug.Log("onvalue Changed|" );
+   }
+}
